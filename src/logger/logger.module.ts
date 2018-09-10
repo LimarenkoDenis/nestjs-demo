7f9e483a407c735b080b4d5ff918313da77147d9
LimarenkoDenis <limarenkodenis@gmail.com>
2018-09-10 20:29:30 +0300

    logger middleware in 30 seconds

---
+++ src/logger/logger.module.ts
@@ -0,0 +1,11 @@
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './logger.middleware';
+
+@Module({})
+export class LoggerModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('users');
+  }
+}
