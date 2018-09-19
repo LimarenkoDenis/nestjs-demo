e9b42109944a1b1416961529d6ee0ccfdf72c6ab
LimarenkoDenis <limarenkodenis@gmail.com>
2018-09-19 12:52:22 +0300

    add validation rules for update user API

--- src/users/dto/update.user.dto.ts
+++ src/users/dto/update.user.dto.ts
@@ -1,6 +1,12 @@
 import { ApiModelProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length, IsEmail } from 'class-validator';
+import { Trim } from 'class-sanitizer';
 
 export class UpdateUserDto {
   @ApiModelProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Length(0, 50)
+  @Trim()
   public readonly username: string;
 }
